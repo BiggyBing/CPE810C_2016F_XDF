441e5cb890290d2ef82723f88c7732d9e4c67779
Seito Ryu <sryu@stevens.edu>
2016-11-04 12:45:26 -0400

    fix coding styple

--- 2dgraphics/2dgraphics.hh
+++ 2dgraphics/2dgraphics.hh
@@ -18,22 +18,32 @@ private:
     static string m_sourceFile;
     //store all source data in SVG format
     vector<string> allObjects;
-    //read source file and set all drawing objects to allObject variable
-    //assume each line in source file represents one object, data is stored by certain rules
-    //The rules of how to store drawing data will be decided in future
+    /**
+        read source file and set all drawing objects to allObject variable
+        assume each line in source file represents one object, data is stored by certain rules
+        The rules of how to store drawing data will be decided in future
+    */
     void setDrawingObjects(){}
 public:
     //constructor
     Draw2DGraphics(){}
-    //in order to get source data, this method will be implemented by other class
+    /**
+        in order to get source data, this method will be implemented by other class
+    */
     static void setDataSource(const string& path_, const string& sourceFile_){
       m_path = path_;
       m_sourceFile = sourceFile_;
     }
-    //set path of source data file
+    /**
+        set path of source data file
+    */
     string getPath() const {return path;}
-    //set name of source data file
+    /**
+        set name of source data file
+    */
     string getsourceFile() const {return sourceFile;}
-    //read from variable allObject and display them on mainwindow
+    /**
+        read from variable allObject and display them on mainwindow
+    */
     virtual void paintEvent(QPaintEvent* event){}
 }
